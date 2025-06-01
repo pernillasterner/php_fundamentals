ac96156b2198a48b73ccb5f0ba2f5cd7e9e46d13
pernillasterner <psterner@hotmail.com>
2025-06-01 14:37:57 +0200

    Add partials for head, nav, footer

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -7,7 +7,7 @@
           </div>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
-              <a href="/" class="<?= ($_SERVER['REQUEST_URI'] === '/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+              <a href="/" class="<?= (urlIs('/')) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
               <a href="/about.php" class="<?= ($_SERVER['REQUEST_URI'] === '/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">About</a>
               <a href="/contact.php" class="<?= ($_SERVER['REQUEST_URI'] === '/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Contact</a>
             </div>
