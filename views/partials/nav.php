5a41b334dff095d503713b19b2c56127fea85988
pernillasterner <psterner@hotmail.com>
2025-06-01 14:50:04 +0200

    Remove filetype from routes

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,8 +8,8 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <a href="/" class="<?= (urlIs('/')) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-              <a href="/about.php" class="<?= ($_SERVER['REQUEST_URI'] === '/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">About</a>
-              <a href="/contact.php" class="<?= ($_SERVER['REQUEST_URI'] === '/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+              <a href="/about" class="<?= ($_SERVER['REQUEST_URI'] === '/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">About</a>
+              <a href="/contact" class="<?= ($_SERVER['REQUEST_URI'] === '/contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> rounded-md px-3 py-2 text-sm font-medium">Contact</a>
             </div>
           </div>
         </div>
@@ -46,8 +46,8 @@
     <div class="md:hidden" id="mobile-menu">
       <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
         <a href="/" class="<?= ($_SERVER['REQUEST_URI'] === '/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
-        <a href="/about.php" class="<?= ($_SERVER['REQUEST_URI'] === '/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium">About</a>
-        <a href="/contact.php" class="<?= ($_SERVER['REQUEST_URI'] === '/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium">Contact</a>
+        <a href="/about" class="<?= ($_SERVER['REQUEST_URI'] === '/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium">About</a>
+        <a href="/contact" class="<?= ($_SERVER['REQUEST_URI'] === '/contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' ?> block rounded-md px-3 py-2 text-base font-medium">Contact</a>
       </div>
     </div>
   </nav>
