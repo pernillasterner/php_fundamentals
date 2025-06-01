6ade8ee5e0da5107d0075c5b35a57b97cc5af2c3
pernillasterner <psterner@hotmail.com>
2025-06-01 14:31:44 +0200

    Remove require functions.php

--- views/index.view.php
+++ views/index.view.php
@@ -1,16 +1,11 @@
 <?php require("partials/head.php"); ?>
-  <?php require("partials/nav.php"); ?>
+<?php require("partials/nav.php"); ?>
+<?php require('partials/banner.php'); ?>   
 
-    <header class="bg-white shadow-sm">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
-      </div>
-    </header>
-
-    <main>
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        
-      </div>
-    </main>
+<main>
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <p>Welcome to the home page</p>
   </div>
+</main>
+
 <?php require("partials/footer.php"); ?>
