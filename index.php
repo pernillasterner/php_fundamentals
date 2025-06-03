fa306b67e5ddc8e3a937ebc8c97f80aebfb38797
pernillasterner <psterner@hotmail.com>
2025-06-03 07:44:51 +0200

    Create db connection

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 
 require('functions.php');
-require('router.php');
+// require('router.php');
+
+
+// connect to MySQL database
+$dns = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dns);
+
+// prepared query statement
+$statement = $pdo->prepare("SELECT * FROM notes");
+$statement->execute();
+
+$notes = $statement->fetchAll();
+
+dd($notes);
