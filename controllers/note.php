10105374b50b4ecfd66faea6510501655dfbd70a
pernillasterner <psterner@hotmail.com>
2025-06-04 10:23:09 +0200

    Get single note from db. Add view to show single note

---
+++ controllers/note.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$heading = 'My Note';
+
+$id = $_GET['id'];
+
+$note = $db->query("select * from notes where id = :id", ['id' => $id])->fetch();
+
+require('views/note.view.php'); 
