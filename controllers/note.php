2fed1e99103013e3fd69ddb5eb2d55620861b433
pernillasterner <psterner@hotmail.com>
2025-06-04 11:01:17 +0200

    Add check to handle permissions for current user

--- controllers/note.php
+++ controllers/note.php
@@ -7,6 +7,19 @@ $heading = 'My Note';
 
 $id = $_GET['id'];
 
-$note = $db->query("select * from notes where id = :id", ['id' => $id])->fetch();
+$note = $db->query("select * from notes where id = :id", [
+  'id' => $id
+])->fetch();
+
+
+if (! $note) {
+  abort();
+}
+
+$currentUserId = 1;
+
+if ($note['user_id'] !== $currentUserId) {
+  abort(403);
+}
 
 require('views/note.view.php'); 
