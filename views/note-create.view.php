177d15a3f61c9747c15f3fb77e0ffb0705652d15
pernillasterner <psterner@hotmail.com>
2025-06-05 11:05:51 +0200

    Add tailwind form

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -6,11 +6,26 @@
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
 
     <!-- Form for creating a note -->
-    <form method="POST" class="flex flex-col gap-4">
-      <label for="body">Add Note</label>
-      <textarea name="body" id="body" rows="4" cols="50" placeholder="Write something here..." class="p-2 text-sm"></textarea>
-      <button type="submit" class="bg-blue-500 text-white rounded-md py-2 px-4 w-fit">Submit</button>
+    <form method="POST">
+      <div class="space-y-12">
+        <div class="border-b border-gray-900/10 pb-12">
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div class="col-span-full">
+              <label for="body" class="block text-sm/6 font-medium text-gray-900">Add a new note</label>
+              <div class="mt-2">
+                <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
+        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+      </div>
     </form>
+
 
     <p class="mt-6">
       <a href="#" class="text-blue-500 hover:underline">Create Note</a>
