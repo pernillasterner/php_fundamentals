70f8557db7d5520b2daa5f88dd27097f5966cfce
pernillasterner <psterner@hotmail.com>
2025-06-05 12:37:55 +0200

    wip

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,11 +7,19 @@ $heading = 'Create Note';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-  $db->query("insert into notes(body, user_id) values (:body, :user_id)", [
-    'body' => $_POST['body'],
-    'user_id' => 1
-  ]);
+  $errors = [];
 
+  if(strlen($_POST['body']) === 0) {
+    $errors['body'] = 'A body is required';
+  }
+
+  if(empty($errors)) {
+    $db->query("insert into notes(body, user_id) values (:body, :user_id)", [
+      'body' => $_POST['body'],
+      'user_id' => 1
+    ]);
+  }
+  
 }
 
 require('views/note-create.view.php'); 
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -10,9 +10,22 @@
         <div class="border-b border-gray-900/10 pb-12">
           <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div class="col-span-full">
-              <label for="body" class="block text-sm/6 font-medium text-gray-900">Add a new note</label>
+              <label for="body" class="block text-sm/6 font-medium text-gray-900">
+                Add a new note
+              </label>
+
               <div class="mt-2">
-                <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                <textarea 
+                  name="body" 
+                  id="body" 
+                  rows="3" 
+                  class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  placeholder="Here´s an idea for a note"
+                ></textarea>
+
+                <?php if(isset($errors['body'])) : ?>
+                  <p class="text-red-500 text-xs mt-2"><?= $errors['body']; ?></p>
+                <?php endif; ?>
               </div>
             </div>
           </div>
@@ -24,10 +37,6 @@
         <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
       </div>
     </form>
-
-    <p class="mt-6">
-      <a href="#" class="text-blue-500 hover:underline">Create Note</a>
-    </p>
   </div>
 </main>
 
