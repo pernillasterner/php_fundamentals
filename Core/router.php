1eb0fd0ae62cefdc8ce5e17ccf79311208d84556
pernillasterner <psterner@hotmail.com>
2025-06-08 10:20:50 +0200

    Group infrastructure code

--- Core/router.php
+++ Core/router.php
@@ -1,11 +1,10 @@
 <?php
 
-$routes = require('routes.php');
 
 function routeToController($uri, $routes) {
   // Check if key exists in route array
   if (array_key_exists($uri, $routes)) {
-    require($routes[$uri]);
+    require base_path($routes[$uri]);
   } else {
     abort();
   }
@@ -15,12 +14,13 @@ function abort($code = 404) {
   
   http_response_code($code);
   
-  require("views/{$code}.php");
+  require base_path("views/{$code}.php");
   
   die();
   
 }
 
+$routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
