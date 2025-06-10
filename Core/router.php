1a7656c2e4c906992bc3c77de028e7773c40d3af
pernillasterner <psterner@hotmail.com>
2025-06-10 12:39:34 +0200

    wip

--- Core/router.php
+++ Core/router.php
@@ -8,40 +8,70 @@ class Router
 
   public function add($uri, $controller, $method)
   {
-    $this->routes[] = compact('uri', 'controller', 'method');
+    $this->routes[] = [
+      'uri' => $uri,
+      'controller' => $controller,
+      'method' => $method,
+      'middleware' => NULL 
+    ];
+
+    return $this;
   }
 
   public function get($uri, $controller)
   {
-    $this->add($uri, $controller, 'GET');
+    return $this->add($uri, $controller, 'GET');
   }
 
   public function post($uri, $controller)
   {
-    $this->add($uri, $controller, 'POST');
+    return $this->add($uri, $controller, 'POST');
   }
 
   public function delete($uri, $controller)
   {
-    $this->add($uri, $controller, 'DELETE');
+    return $this->add($uri, $controller, 'DELETE');
   }
 
   public function patch($uri, $controller)
   {
-    $this->add($uri, $controller, 'PATCH');
+    return $this->add($uri, $controller, 'PATCH');
   }
 
   public function put($uri, $controller)
   {
-    $this->add($uri, $controller, 'PUT');
+    return $this->add($uri, $controller, 'PUT');
+  }
+
+  public function only($key)
+  {
+    $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+
+    return $this;
   }
 
   public function route($uri, $method)
   {
     // Check if key exists in route array
     foreach($this->routes as $route) {
-
+   
       if($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+        // check if page is for guest and user not logged in
+        if($route['middleware'] === 'guest') {
+          // if user is logged in then send to homepage
+          if($_SESSION['user'] ?? false) {
+            header('Location: /');
+            exit();
+          }
+        }
+
+        if($route['middleware'] === 'auth') {
+          // if user is logged in then send to homepage
+          if(! $_SESSION['user'] ?? false) {
+            header('Location: /');
+            exit();
+          }
+        }
         return require base_path($route['controller']);
       }
     }
